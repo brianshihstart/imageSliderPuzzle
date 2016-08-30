4035a531692fd22aa89664cd934c0f521e46fdd4
Brian Shih <brianshihstart@gmail.com>
2016-08-31 00:08:53 +0800

    "add board and tiles class"

---
+++ imageSlider/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  imageSlider
+//
+//  Created by Brian Shih on 8/30/16.
+//  Copyright © 2016 BrianShih. All rights reserved.
+//
+
+import UIKit
+
+class Board: UIView {
+    
+}
