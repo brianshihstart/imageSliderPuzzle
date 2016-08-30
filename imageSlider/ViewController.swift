5cde8a57088e9d51b095bd288f1701469e7a30c3
Brian Shih <brianshihstart@gmail.com>
2016-08-30 21:49:02 +0800

    Changed Background

--- imageSlider/ViewController.swift
+++ imageSlider/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        view.backgroundColor = UIColor.redColor()
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
